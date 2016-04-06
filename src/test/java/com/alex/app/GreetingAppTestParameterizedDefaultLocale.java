5ebfa26219a30f902ab48479a1973ca8ea3cf5d5
Alex <neophyteinpast@gmail.com>
2016-04-06 09:54:47 +0300

    Added files via upload

---
+++ src/test/java/com/alex/app/GreetingAppTestParameterizedDefaultLocale.java
@@ -0,0 +1,49 @@
+package com.alex.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class GreetingAppTestParameterizedDefaultLocale {
+
+    static Locale locale;
+    static ResourceBundle bundle;
+    GreetingApp greetingApp = new GreetingApp();
+
+    private String input;
+    private String expectedOutput;
+
+    public GreetingAppTestParameterizedDefaultLocale(String input, String expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<String[]> testConditions() {
+        String[][] expectedValues = {{"morning", "Доброе утро, Мир!"}, {"day", "Добрый день, Мир!"}, {"evening", "Добрый вечер, Мир!"}, {"night", "Доброй ночи, Мир!"}};
+        return Arrays.asList(expectedValues);
+    }
+
+    @Test
+    public void displayMessage() {
+
+        locale = Locale.getDefault();
+
+        if (locale.getISO3Country().equals("RUS")) {
+            bundle = ResourceBundle.getBundle("i18n.MessageResource", new Utf8Control());
+        }
+        else {
+            bundle = ResourceBundle.getBundle("i18n.MessageResource", locale);
+        }
+
+        assertEquals(expectedOutput, greetingApp.generateMessage(bundle, input));
+    }
+}
